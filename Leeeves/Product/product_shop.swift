19f0f0cd07245354b1bea68160829421d869fd72
maciura <kosmit1001@gmail.com>
2024-05-04 10:28:52 +0200

    product_shop, not finish

---
+++ Leeeves/Product/product_shop.swift
@@ -0,0 +1,18 @@
+//
+//  product_shop.swift
+//  Leeeves
+//
+//  Created by maciura on 04/05/2024.
+//
+
+import SwiftUI
+
+struct product_shop: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    product_shop()
+}
