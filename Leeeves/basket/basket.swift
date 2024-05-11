be0859310c83ec1ec36634e9aec611ab77e4d4e4
maciura <kosmit1001@gmail.com>
2024-05-11 14:57:58 +0200

    Create Basket View- not finished

---
+++ Leeeves/basket/basket.swift
@@ -0,0 +1,18 @@
+//
+//  basket.swift
+//  Leeeves
+//
+//  Created by maciura on 09/05/2024.
+//
+
+import SwiftUI
+
+struct basket: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    basket()
+}
